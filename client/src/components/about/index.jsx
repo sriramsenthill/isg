153c8decf4a895403583a9d08d22fe170c8eb731
sriramsenthill <sriram.senthilnathan@gmail.com>
2024-01-01 01:25:12 +0530

    err try 2

--- client/src/components/about/index.jsx
+++ client/src/components/about/index.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import FeatureArea from "../../common/feature-area";
-import AboutArea from "../../common/about-area";
-import BrandArea from "@/src/common/brand-area";
-import VideoArea from "../../common/video-area";
-import ChooseArea from "../../common/choose-area";
-import TestimonialAreaThree from "../../common/testimonial-area-2";
-import InstructorArea from "./instructor-area";
-import SuitableArea from "@/src/common/suitable-area";
-import CounterArea from "../homes/home-3/counter-area";
 import Breadcrumb from "../bredcrumb/breadcrumb";
 import OurVision from "../homes/home/OurVision";
-import OurMission from "../homes/home/ourMission";
+import OurMission from "../homes/home/OurMission";
 import OurFaith from "../homes/home/OurFaith";
 import OurFaithCreed from "../homes/home/OurFaithCreed";
 const About = () => {
