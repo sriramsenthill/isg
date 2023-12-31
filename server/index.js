1b8452fc5b6b22562581986cbb52d0d73cd0a986
sriramsenthill <sriram.senthilnathan@gmail.com>
2024-01-01 01:41:10 +0530

    server changes

--- server/index.js
+++ server/index.js
@@ -1,16 +1,41 @@
-// Importing required modules
-const express = require('express');
-
-// Creating an Express application
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
-const port = 3000; // You can use any port number you prefer
+app.use(express.json());
+app.use(cors());
+const port = 3000;
 
-// Define a route for the root endpoint
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+// Connect to your MongoDB database
+mongoose
+    .connect(
+        "mongodb+srv://isg:isguni@cluster0.zowuscz.mongodb.net/admin?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
+
+app.get("/", async (req, res) => {
+    try {
+        res.send("hello world");
+    } catch (error) {
+        console.error("Error fetching job orders:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
-// Start the server and listen on the specified port
+
+
+
+// Start the server
+
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+    console.log(`Server is running on port ${port}`);
+}); 
