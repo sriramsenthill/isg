65982a99d5772cdd5da70e5afbfff6528a720f47
sriramsenthill <sriram.senthilnathan@gmail.com>
2024-01-07 23:36:28 +0530

    build fix

--- client/src/pages/prof/ben.jsx
+++ client/src/pages/prof/ben.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Breadcrumb from "@/src/components/bredcrumb/breadcrumb";
-import Ben from "../../components/instructor-profile/professor/Ben";
+import Ben from "../../components/instructor-profile/professor/ben"
 import Wrapper from "@/src/layout/wrapper";
 
 const proffour = () => {
