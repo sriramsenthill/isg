69f100e7a8df66955c7f3f6a483c8c3a34850658
sriramsenthill <sriram.senthilnathan@gmail.com>
2024-01-15 01:13:31 +0530

    changes in server name

--- client/src/pages/sign-in.jsx
+++ client/src/pages/sign-in.jsx
@@ -4,15 +4,6 @@ import SignIn from "../components/sign-in";
 import WrapperFour from "../layout/wrapper-4";
 
 const Index = () => {
-  useEffect(() => {
-    // Check for existing JWT token
-    const jwtToken = localStorage.getItem('jwtToken');
-
-    // If token exists, clear it
-    if (jwtToken) {
-      localStorage.removeItem('jwtToken');
-    }
-  }, []);
 
   return (
     <>
